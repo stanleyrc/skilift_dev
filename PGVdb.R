10241be2a05c83a72edff152eaeabbf8d7847c79
Shihab Dider <shihabdider@gmail.com>
2023-07-18 15:01:00 -0400

    feat: Fix add, remove and validate methods
    Methods were breaking due to edges cases (higlass plots) and
    insufficient type checking (column names of data.tables)

--- PGVdb.R
+++ PGVdb.R
@@ -1,6 +1,7 @@
 library(R6)
 library(jsonlite)
 library(data.table)
+devtools::load_all('gGnome')
 
 #' @name cov2arrowPGV
 #' @description
@@ -17,63 +18,63 @@ library(data.table)
 #' @param bin.width (integer) bin width for rebinning the coverage (default: 1e4)
 #' @author Alon Shaiber, Max Chao
 #' @export
-cov2arrowPGV <- function(cov,
-                         field = "ratio",
-                         output_file = "coverage.arrow",
-                         ref = "hg19",
-                         meta.js = NULL,
-                         ...) {
-  if (!file.exists(output_file)) {
-    if (!requireNamespace("arrow", quietly = TRUE)) {
-      stop('You must have the package "arrow" installed in order for converting a
-           coverage file to arrow file to work. Please install it.')
-    }
-    message("Converting coverage format")
-    dat <- cov2cov.js(cov,
-      meta.js = meta.js,
-      js.type = "PGV", field = field,
-      ref = ref, ...
-    )
-    message("Done converting coverage format")
-    if (!is.null(meta.js)) {
-      ref_meta <- get_ref_metadata_from_PGV_json(meta.js, ref)
-      setkey(ref_meta, "chromosome")
-      # create a map
-      # 3.981s
-      map_cols <- data.table(
-        color = unique(ref_meta$color),
-        numcolor = color2numeric(unique(ref_meta$color))
-      )
-      dat$color <- merge(ref_meta[dat$seqnames], map_cols,
-        by = "color", sort = FALSE
-      )$numcolor
-    } else {
-      # no cov.color.field and no meta.js so set all colors to black
-      dat$color <- 0
-    }
-    outdt <- dat[, .(x = new.start, y = get(field), color)]
-    # if there are any NAs for colors then set those to black
-    outdt[is.na(color), color := 0]
-    # remove NAs
-    outdt <- outdt[!is.na(y)]
-
-    # sort according to x values (that is what PGV expects)
-    outdt <- outdt[order(x)]
-
-    message("Writing arrow file (using write_feather)")
-    arrow_table <- arrow::Table$create(outdt,
-      schema = arrow::schema(
-        x = arrow::float32(),
-        y = arrow::float32(),
-        color = arrow::float32()
-      )
-    )
-    arrow::write_feather(arrow_table, output_file)
-  } else {
-    message('arrow file, "', output_file, '" already exists.')
-  }
-  return(output_file)
-}
+# cov2arrowPGV <- function(cov,
+#                          field = "ratio",
+#                          output_file = "coverage.arrow",
+#                          ref = "hg19",
+#                          meta.js = NULL,
+#                          ...) {
+#   if (!file.exists(output_file)) {
+#     if (!requireNamespace("arrow", quietly = TRUE)) {
+#       stop('You must have the package "arrow" installed in order for converting a
+#            coverage file to arrow file to work. Please install it.')
+#     }
+#     message("Converting coverage format")
+#     dat <- cov2cov.js(cov,
+#       meta.js = meta.js,
+#       js.type = "PGV", field = field,
+#       ref = ref, ...
+#     )
+#     message("Done converting coverage format")
+#     if (!is.null(meta.js)) {
+#       ref_meta <- get_ref_metadata_from_PGV_json(meta.js, ref)
+#       setkey(ref_meta, "chromosome")
+#       # create a map
+#       # 3.981s
+#       map_cols <- data.table(
+#         color = unique(ref_meta$color),
+#         numcolor = color2numeric(unique(ref_meta$color))
+#       )
+#       dat$color <- merge(ref_meta[dat$seqnames], map_cols,
+#         by = "color", sort = FALSE
+#       )$numcolor
+#     } else {
+#       # no cov.color.field and no meta.js so set all colors to black
+#       dat$color <- 0
+#     }
+#     outdt <- dat[, .(x = new.start, y = get(field), color)]
+#     # if there are any NAs for colors then set those to black
+#     outdt[is.na(color), color := 0]
+#     # remove NAs
+#     outdt <- outdt[!is.na(y)]
+#
+#     # sort according to x values (that is what PGV expects)
+#     outdt <- outdt[order(x)]
+#
+#     message("Writing arrow file (using write_feather)")
+#     arrow_table <- arrow::Table$create(outdt,
+#       schema = arrow::schema(
+#         x = arrow::float32(),
+#         y = arrow::float32(),
+#         color = arrow::float32()
+#       )
+#     )
+#     arrow::write_feather(arrow_table, output_file)
+#   } else {
+#     message('arrow file, "', output_file, '" already exists.')
+#   }
+#   return(output_file)
+# }
 
 
 PGVdb <- R6Class("PGVdb",
@@ -147,37 +148,54 @@ PGVdb <- R6Class("PGVdb",
       # Build the JSON structure
       json_data <- list()
 
-      # Add metadata section
-      metadata <- list(
-        "description" = self$metadata$tags[[1]],
-        "reference" = self$metadata$ref
-      )
-      json_data$DEMO <- metadata
+      # Iterate over patient.ids
+      for (patient_id in self$metadata$patient.id) {
+        # Subset metadata for the current patient
+        metadata <- self$metadata[self$metadata$patient.id == patient_id, ]
+        description <- metadata$tags[[1]]
+        reference <- metadata$ref
 
-      # Add plots section
-      plots <- list()
-      for (i in seq_len(nrow(self$plots))) {
-        plot_entry <- list(
-          "type" = self$plots$type[i],
-          "visible" = self$plots$visible[i],
-          "source" = self$plots$source[i],
-          "title" = self$plots$title[i]
+        # Subset plots for the current patient
+        patient_plots <- self$plots[self$plots$patient.id == patient_id, ]
+        plots <- list()
+        for (i in seq_len(nrow(patient_plots))) {
+          plot_entry <- list(
+            "type" = patient_plots$type[i],
+            "visible" = patient_plots$visible[i]
+          )
+
+          if (!is.na(patient_plots$title[i])) {
+            plot_entry$title <- patient_plots$title[i]
+          }
+          if (!is.na(patient_plots$source[i])) {
+            plot_entry$source <- patient_plots$source[i]
+          }
+          if (!is.na(patient_plots$sample[i])) {
+            plot_entry$sample <- patient_plots$sample[i]
+          }
+          if (!is.na(patient_plots$figure[i])) {
+            plot_entry$figure <- patient_plots$figure[i]
+          }
+          if (!is.na(patient_plots$server[i])) {
+            plot_entry$server <- patient_plots$server[i]
+          }
+          if (!is.na(patient_plots$uuid[i])) {
+            plot_entry$uuid <- patient_plots$uuid[i]
+          }
+
+          plots[[i]] <- plot_entry
+        }
+
+        # Create patient data entry
+        patient_data <- list(
+          "description" = description,
+          "reference" = reference,
+          "plots" = plots
         )
-        if (!is.na(self$plots$sample[i])) {
-          plot_entry$sample <- self$plots$sample[i]
-        }
-        if (!is.na(self$plots$figure[i])) {
-          plot_entry$figure <- self$plots$figure[i]
-        }
-        if (!is.na(self$plots$server[i])) {
-          plot_entry$server <- self$plots$server[i]
-        }
-        if (!is.na(self$plots$uuid[i])) {
-          plot_entry$uuid <- self$plots$uuid[i]
-        }
-        plots[[i]] <- plot_entry
+
+        # Add the patient data to the main json_data list
+        json_data[[patient_id]] <- patient_data
       }
-      json_data$DEMO$plots <- plots
 
       # Convert JSON to string and write to file
       json_string <- jsonlite::toJSON(json_data, auto_unbox = TRUE, pretty = TRUE)
@@ -211,36 +229,53 @@ PGVdb <- R6Class("PGVdb",
       new_plots <- data.table::setDT(plots_to_add) # Convert to data.table if required
 
       # Check if required columns exist
-      required_columns <- c("patient.id", "sample", "type", "source", "path", "visible")
-      if (!all(required_columns %in% names(new_plots))) {
+      required_columns <- c("patient.id", "type", "path", "visible")
+      empty <- data.table::data.table(
+        patient.id = character(),
+        type = character(),
+        source = character(),
+        server = character(),
+        uuid = character(),
+        path = character(),
+        visible = logical(),
+        stringsAsFactors = FALSE
+      )
+      if (!all(required_columns %in% names(new_plots)) &&
+      !(("source" %in% names(new_plots)) || ("server" %in% names(new_plots) && "uuid" %in% names(new_plots)))) {
         print("Required columns not found, creating an empty data.table with required columns instead...")
-        return(data.table::data.table(patient.id = character(),
-          sample = character(),
-          type = character(),
-          source = character(),
-          path = character(),
-          visible = logical(),
-          stringsAsFactors = FALSE))
+        print("(All plots must have either a non-null source field or non-null server and uuid fields)")
+
+        return(empty)
+      }
+
+      if (any(!new_plots$patient.id %in% self$metadata$patient.id)) {
+        if (!("ref" %in% names(new_plots)) | is.na(new_plots$ref) | is.null(new_plots$ref)) {
+          print("You are trying to add a new patient without specifying its reference. Make sure all references are non-null")
+          print("Creating an empty data.table with required columns instead...")
+          empty$ref <- character()
+          return(empty)
+        }
       }
 
       # Loop through each row of the plots_to_add table
       for (i in seq_len(nrow(new_plots))) {
         plot <- new_plots[i, ]
 
-        # Check if patient exists, if not, create patient directory
+        # Check if patient exists, if not, make sure ref exists, then create patient directory
         patient_dir <- file.path(self$datadir, plot$patient.id)
         if (!dir.exists(patient_dir)) {
+          print(paste("Patient directory does not exist. Creating a directory for new patient: ", plot$patient.id))
           dir.create(patient_dir)
         }
 
         # Check if plot source does not exist for that patient or if overwrite = TRUE
         plot_file <- file.path(patient_dir, plot$source)
         if (file.exists(plot_file) && !overwrite_files) {
-          message(paste0("Plot source file already exists for patient ", plot$patient.id, ". Specify overwrite_files = TRUE to overwrite."))
+          message(paste0("Plot source file already exists for patient ", plot$patient.id, ". Set overwrite_files = TRUE to overwrite."))
         } else {
           # Use the plot type to determine the conversion function to use
           if (plot$type == "genome") {
-            self$create_ggenome_json(plot)
+            self$create_ggraph_json(plot)
           } else if (plot$type == "scatterplot") {
             self$create_cov_arrow(plot)
           } else if (plot$type == "walk") {
@@ -250,6 +285,36 @@ PGVdb <- R6Class("PGVdb",
           # Save plot to patient directory
           if (file.exists(plot$path)) {
             file.copy(plot$path, plot_file, overwrite = TRUE)
+          } else {
+            print("Plot file does not exist. Please check that the path to the plot file is correct.")
+          }
+        }
+
+        common_columns <- intersect(names(plot), names(self$plots))
+        extended_data <- plot[, ..common_columns]
+
+        is_duplicate <- duplicated(rbind(self$plots, extended_data, fill=TRUE))
+
+        if (any(is_duplicate)) {
+          message("Skipping duplicate row: ")
+          print(plot)
+        } else {
+          self$plots <- rbind(self$plots, extended_data, fill = TRUE)
+        }
+
+        if (plot$patient.id %in% self$metadata$patient.id) {
+          # If patient.id exists, update tags and ref
+          if ("tags" %in% colnames(plot)) {
+            self$metadata[self$metadata$patient.id == plot$patient.id, "tags"] <- plot$tags
+          }
+          if ("ref" %in% colnames(plot) & !is.null(plot$ref) & !is.na(plot$ref) ) {
+            self$metadata[self$metadata$patient.id == plot$patient.id, "ref"] <- plot$ref
+          }
+        } else {
+          if ("tags" %in% colnames(plot)) {
+            self$metadata <- rbind(self$metadata, data.frame(patient.id = plot$patient.id, tags = plot$tags, ref = plot$ref), fill = TRUE)
+          } else {
+            self$metadata <- rbind(self$metadata, data.frame(patient.id = plot$patient.id, ref = plot$ref), fill = TRUE)
           }
         }
       }
@@ -262,23 +327,25 @@ PGVdb <- R6Class("PGVdb",
       remove_plots <- data.table::setDT(plots_to_remove) # Convert to data.table if required
 
       # Determine if only patient IDs are provided
-      only_patient_ids <- "patient.id" %in% names(remove_plots) && !(c("sample", "source") %in% names(remove_plots))
+      is_remove_patients <- length(names(remove_plots)) == 1 && names(remove_plots) == "patient.id"
+      is_remove_server <- "server" %in% names(remove_plots)
 
       # Check if required columns exist
-      required_columns <- if (only_patient_ids) "patient.id" else c("patient.id", "sample", "source")
-      if (!all(required_columns %in% names(remove_plots))) {
-        return("Error: Required columns (patient.id OR patient.id, source, sample) not found in plots_to_remove data.table.")
+      required_columns <- if (is_remove_patients) "patient.id" else c("patient.id", "source")
+      server_required_columns <- c("patient.id", "server", "uuid")
+      if (!(all(required_columns %in% names(remove_plots)) || all(server_required_columns %in% names(remove_plots)))) {
+        return("Error: Required columns (patient.id OR patient.id, source OR patient.id, server, uuid) not found in plots_to_remove data.table.")
       }
 
       # Remove plots from PGVdb$plots
-      if (only_patient_ids) {
+      if (is_remove_patients) {
         self$plots <- self$plots[!patient.id %in% remove_plots$patient.id]
       } else {
         self$plots <- self$plots[!(patient.id %in% remove_plots$patient.id & source %in% remove_plots$source)]
+        if (is_remove_server) {
+          self$plots <- self$plots[!(patient.id %in% remove_plots$patient.id & server %in% remove_plots$server)]
+        }
       }
-
-      # Update datafiles.json
-      self$update_datafiles_json()
 
       # Call validate to remove any patients that have no plots
       self$validate()
@@ -286,11 +353,11 @@ PGVdb <- R6Class("PGVdb",
       # If delete_files is TRUE, remove plot files and patient directories if empty
       if (delete_files) {
         # Loop through each row of the plots_to_remove table
-        for (i in seq_len(nrow(remove_plots))) {
+        for (i in seq_len(nrow(remove_plots[!is.na("source")]))) {
           plot <- remove_plots[i, ]
 
           # Remove plot files
-          if (only_patient_ids) {
+          if (is_remove_patients) {
             patient_plots <- self$plots[patient.id == plot$patient.id, .(patient.id, source)]
           } else {
             patient_plots <- self$plots[patient.id == plot$patient.id & source %in% plot$source, .(patient.id, source)]
@@ -323,17 +390,32 @@ PGVdb <- R6Class("PGVdb",
         self$metadata <- self$metadata[!patient.id %in% patients_without_plots]
       }
 
-      # Check if all source files exist in proper directory
-      missing_files <- self$plots[!file.exists(file.path(self$datadir, patient.id, source)), .(patient.id, source)]
+      # Check if all source files exist in proper directory and server is not null
+      missing_files <- self$plots[(is.na(server) | is.null(server)) & !file.exists(file.path(self$datadir, patient.id, source)), .(patient.id, source)]
 
-      # Check if all plots have required column values != NA or NULL
-      missing_values <- self$plots[sapply(.SD, function(x) any(is.na(x) | is.null(x))), .SDcols = c("patient.id", "source")]
+      missing_servers <- self$plots[(is.na(source) | is.null(source)) & (is.na(server) | is.null(server) & (is.na(uuid) | is.null(uuid)))]
+      # All plots must have a patient.id and either a source or server and uuid
+      missing_values <- self$plots[
+        (is.na(patient.id) | is.null(patient.id)) |
+        (
+          (is.na(source) | is.null(source)) &
+          (
+            (is.na(server) | is.null(server)) &
+            (is.na(uuid) | is.null(uuid))
+          )
+        ),
+        .SDcols = c("patient.id", "source", "server", "uuid"),
+      ]
 
       # Construct error message with missing files and missing values
       error_message <- ""
       if (nrow(missing_files) > 0) {
         error_message <- paste(error_message, "Missing Files:\n")
         error_message <- paste(error_message, paste(missing_files$patient.id, missing_files$source, sep = " - "), collapse = "\n")
+      }
+      if (nrow(missing_servers) > 0) {
+        error_message <- paste(error_message, "Missing Servers (or uuids for the servers):\n")
+        error_message <- paste(error_message, paste(missing_files$patient.id, missing_files$server, missing_files$uuid, sep = " - "), collapse = "\n")
       }
       if (nrow(missing_values) > 0) {
         error_message <- paste(error_message, "Missing Values:\n")
@@ -342,7 +424,9 @@ PGVdb <- R6Class("PGVdb",
 
       # Return error message if there are any missing files or values
       if (error_message != "") {
-        return(error_message)
+        print(error_message)
+      } else {
+        self$update_datafiles_json()
       }
     },
 
@@ -369,7 +453,11 @@ PGVdb <- R6Class("PGVdb",
     },
 
     create_ggraph_json = function(plot_metadata, overwrite = FALSE) {
-      ggraph_json_path <- file.path(self$datadir, plot_metadata$source)
+      ggraph_json_path <- file.path(
+        self$datadir,
+        plot_metadata$patient.id,
+        plot_metadata$source
+      )
       if (!file.exists(ggraph_json_path) || overwrite) {
         print(paste0("reading in ", plot_metadata$path))
         # TODO: at some point we need to do a sanity check to see that a valid rds of gGraph was provided
@@ -386,7 +474,7 @@ PGVdb <- R6Class("PGVdb",
             ref = plot_metadata$ref
           )
           # check for overlap in sequence names
-          ggraph.reduced <- gg[seqnames %in% names(seq_lengths)]
+          ggraph.reduced <- ggraph[seqnames %in% names(seq_lengths)]
           if (length(ggraph.reduced) == 0) {
             stop(sprintf(
               'There is no overlap between the sequence names in the reference
@@ -397,7 +485,7 @@ PGVdb <- R6Class("PGVdb",
             ))
           }
           # sedge.id or other field
-          if (plot_metadata$annotation) {
+          if ("annotation" %in% colnames(plot_metadata)) {
             # probably check for other cid.field names?
             # field = 'sedge.id'
             refresh(ggraph[seqnames %in% names(seq_lengths)])$json(
@@ -450,7 +538,8 @@ publicdir <- "~/projects/pgv/public"
 settings <- "~/projects/pgv/public/settings.json"
 
 db <- PGVdb$new(datafiles.json, datadir, publicdir, settings)
-# db$drop_patient("TEST")
-db$update_datafiles_json()
+# db$add_plots(data.table(patient.id="TEST", sample="1", ref="hg19", type="genome", path="HCC1954.gg.rds", source="genome.json", visible=TRUE))
+# db$remove_plots(data.table(patient.id = "TEST"))
+# # db$update_datafiles_json()
 # writeLines(db$to_json(), "test.datafiles.json")
 # filtered_patients <- db$filter_by_patient_id("E")
