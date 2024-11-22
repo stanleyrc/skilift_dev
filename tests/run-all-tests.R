ef1025ce9490762b9da0794341eb898ee7bca974
Shihab Dider <shihabdider@gmail.com>
2024-11-22 10:26:46 -0500

    init: signatures lifter

--- tests/run-all-tests.R
+++ tests/run-all-tests.R
@@ -4,13 +4,15 @@ library(testthat)
 
 # test_all <- function() { testthat::test_file("tests/run-all-tests.R") }
 
-testthat::test_file("tests/testthat/test-CaseReportsData.R")
-testthat::test_file("tests/testthat/test-Cohort.R")
-testthat::test_file("tests/testthat/test-copy-number-graph.R")
-testthat::test_file("tests/testthat/test-filtered-events.R")
-testthat::test_file("tests/testthat/test-metadata.R")
-testthat::test_file("tests/testthat/test-scatterplot.R")
-testthat::test_file("tests/testthat/test-segment-width-distribution.R")
-testthat::test_file("tests/testthat/test-variant-qc.R")
+testthat::test_dir("tests/testthat", reporter = testthat::ListReporter)
+
+# testthat::test_file("tests/testthat/test-CaseReportsData.R")
+# testthat::test_file("tests/testthat/test-Cohort.R")
+# testthat::test_file("tests/testthat/test-copy-number-graph.R")
+# testthat::test_file("tests/testthat/test-filtered-events.R")
+# testthat::test_file("tests/testthat/test-metadata.R")
+# testthat::test_file("tests/testthat/test-scatterplot.R")
+# testthat::test_file("tests/testthat/test-segment-width-distribution.R")
+# testthat::test_file("tests/testthat/test-variant-qc.R")
 
 
